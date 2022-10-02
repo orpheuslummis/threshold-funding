969553cf57a62c0f8d8d93dac95e22b709df2e2f
Orpheus Lummis <o@orpheuslummis.info>
2022-10-02 09:30:07 -0500

    init

---
+++ src/bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use threshold_action::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
